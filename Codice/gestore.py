a9d31fab34edc99b1f48588895eca998248bed8f
loretor <lorenzo.torri@virgilio.it>
2022-05-05 14:52:33 +0200

    attività thread con media e liste funzionanti

--- Codice/gestore.py
+++ Codice/gestore.py
@@ -1,5 +1,5 @@
 from threading import Thread, RLock
-import time, queue
+import time, queue, random
 
 #classe custom per gestire la queue dei thread che richiedono un lock condiviso
 class pila:
@@ -36,11 +36,12 @@ class pila:
     def length(self):
         return len(self.Elementi)
 
+
 #variabili globali
 mutex = RLock()
 q = pila()
 
-#classe Thread paralleli 
+#classe Thread paralleli
 class Thread_paralleli(Thread):
     def __init__(self, activity, identificativo):
         Thread.__init__(self)
@@ -51,7 +52,10 @@ class Thread_paralleli(Thread):
         global mutex
         global q
 
-        while True:
+        #la i serve solamente per simulare il funzionamento fino a 7 per poi controllare la console facilemnte
+        i = 0
+
+        while(i <= 7):
             mutex.acquire()
 
             #se siamo nella situazione di lista vuota dobbiamo inserire il thread nella lista di attesa
@@ -76,58 +80,73 @@ class Thread_paralleli(Thread):
                         q.push(self.identificativo)
 
             mutex.release()
+            i+=1
             #la dormita è solo per rendere la simulazione più lenta se no non si vede niente
             time.sleep(2)
 
-#classe thread compiti (sono dei thread sensori e svolgono le attività--> ventola/pompa)
 
-
-#liste per fare una media di un certo numero di valori e non considerare solo il singolo 
-lista_valori_dht22_temperatura = queue.Queue(40)
-lista_valori_dht22_umidita = queue.Queue(40)
-lista_valori_capacitive = queue.Queue(40)
+#liste per fare una media di un certo numero di valori e non considerare solo il singolo
+lista_valori_dht22_temperatura = queue.Queue(5)
+lista_valori_dht22_umidita = queue.Queue(5)
+lista_valori_capacitive = queue.Queue(5)
 
 #valori di riferimento per prendere decisioni
 M_temperatura_aria = 0
-M_umidita_aria_max = 0
-M_umidita_aria_min = 0
+M_umidita_aria = 0
 M_umidita_suolo = 0
-N_irrigazioni = 0 #serve per capire dopo qaunti cicli di acqua si fertilizza
+
 
 #metodo per aggiornare il valore di una media e la lista degli ultimi valori letti da un sensore
 def update_M(lista, M, valore):
+    media = M
     #se la lista è piena bisogna togliere il più vecchio valore letto (politica FIFO) e aggiornare la media
     if lista.full():
         y = lista.get()
-        M = (lista.qsize() * M - y) / (lista.qsize() - 1)
+        media = ((lista.qsize()+1) * media - y) / (lista.qsize())
+
     #se la lista non è piena o è terminato l'if basta aggiungere il valore e aggiornare la media
     lista.put(valore)
-    M = ((lista.qsize() - 1) * M + valore) / lista.qsize()
+    if (lista.qsize() == 0):
+        media = valore
+    else:
+        media = ((lista.qsize() - 1) * media + valore) / lista.qsize()
+
+    return media
 
 
-#funzione attività del sensore DTH22, per misura umidità aria e temperatura
+## funzione attività del sensore DTH22, per misura umidità aria e temperatura
 def activity_DHT22():
-    #corpo
+    global M_temperatura_aria
+    global M_umidita_aria
+
     print('DHT22')
+    #aggiorno la lista_valori_dht22_temperatura e la M_temperatura_aria
+    valore = random.randint(0,22)
+    M_temperatura_aria = update_M(lista_valori_dht22_temperatura, M_temperatura_aria, valore)
+    M_umidita_aria = update_M(lista_valori_dht22_umidita, M_umidita_aria, valore)
+    printqueue(list(lista_valori_dht22_umidita.queue))
+    print("M: "+str(M_temperatura_aria))
+    print("--------------------")
+    printqueue(list(lista_valori_dht22_temperatura.queue))
+    print("M: "+str(M_umidita_aria))
+    print("--------------------")
 
-#funzione attività del sensore HCSR04, per distanza da galleggiante
-def activity_HCSR04():
-    #corpo
-    print('HCSR04')
-    
-#funzione attività del sensore Capacitive Soil Moisture, per umidità suolo
+
+## funzione attività del sensore Capacitive Soil Moisture, per umidità suolo
 def activity_Capacitive():
-    #corpo
+    global M_umidita_suolo
+
     print('CAPACITIVE')
+    valore = random.randint(0,100)
+    M_umidita_suolo = update_M(lista_valori_capacitive, M_umidita_suolo, valore)
+    printqueue(list(lista_valori_capacitive.queue))
+    print("M: "+str(M_umidita_suolo))
+    print("--------------------")
 
 
-#MAIN
 t1 = Thread_paralleli(activity = activity_DHT22, identificativo = "DHT22" )
 t2 = Thread_paralleli(activity = activity_Capacitive, identificativo = "Capacitive" )
 #hcsr04 deve essere un thread figlio di irrigazione (controllo livello dopo aver irrigato)
-#t3 = Thread_paralleli(activity = activity_HCSR04(), identificativo = "HCSR04" )
-    
+
 t1.start()
 t2.start()
-#t3.start()
-
