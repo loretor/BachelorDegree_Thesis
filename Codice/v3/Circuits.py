5a737b606971419b9bb7bcd95422c189c7f2b097
Lorenzo Torri <68181508+loretor@users.noreply.github.com>
2022-07-13 16:33:31 +0200

    Update Circuits.py

--- Codice/v3/Circuits.py
+++ Codice/v3/Circuits.py
@@ -8,6 +8,10 @@ import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
+# librerie per hcsr04
+import RPi.GPIO as GPIO
+import time
+
 
 #per lettura valori del sensore DHT22
 def dht22():
@@ -39,3 +43,48 @@ def capacitive():
     umidita = (map(chan1.voltage, 2.990091, 0.604018, 40, 100))
 
     return umidita
+
+def hcsr():
+    GPIO.setmode(GPIO.BCM)
+
+    TRIG = 20
+    ECHO = 16
+    
+    count = 5
+    somma = 0
+    i = 0
+    
+    while i < count:
+        GPIO.setwarnings(False)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
+
+        # siamo sicuri che il trigger sia a false
+        GPIO.output(TRIG, False)
+        print("Waiting for sensor to settle")
+        time.sleep(2)
+
+        # il segnale in ingresso necessita di un impulso di 10uS
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+            
+        # calcolo del tempo che impiega il segnale ad andare e tornare contro l'oggetto
+        pulse_duration = pulse_end - pulse_start
+
+        # velocità del suono è uguale a 34300 cm/s e il tempo è metà in quanto deve fare andata e ritorno
+        distance = pulse_duration * 34300/2
+        distance = round(distance, 2)
+        
+        i += 1
+        somma += distance
+    
+    return somma/count
+        
+
